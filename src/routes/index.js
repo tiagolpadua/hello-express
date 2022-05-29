6f0873612539059d767bd530b096affcb4c0d013
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2022-05-29 18:09:21 -0300

    fix sonar

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (_req, res, next) {
+router.get('/', function (_req, res, _next) {
   res.render('index', { title: 'Express' });
 });
 
